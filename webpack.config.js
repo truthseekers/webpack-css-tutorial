fa0df7360c231b8e39bbd33f9eb0257abc15a49c
John Curry <fastpenguin91@gmail.com>
2021-11-26 19:04:20 -0800

    css generation simple

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // module.exports = {
 //   entry: "./src/index.js",
@@ -17,11 +18,16 @@ module.exports = {
     filename: "[name].js",
     // path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
