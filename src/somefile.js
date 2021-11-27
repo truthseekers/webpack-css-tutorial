f276ad0baab91dc0f9035d12df65463e87f53ec3
John Curry <fastpenguin91@gmail.com>
2021-11-26 19:06:15 -0800

    starting point

--- src/somefile.js
+++ src/somefile.js
@@ -2,4 +2,4 @@ console.log("hello from somefile.js");
 /* when the import style line below is commented, webpack won't create the css file in /dist because
 no css file is being seen. If you uncomment the line, it will generate a css file because a css file is being used inside
 a file that is being watched. (see entry object in webpack file) */
-import "./style.css";
+//import "./style.css";
